9fab4e1b797f256ec9ae9b883ad72c1f92135c78
OnlineTrainer <mohamedtechdata@gmail.com>
2021-11-11 08:11:44 +0530

    11-11-2021

---
+++ ServletConfigP1/src/Servlet1.java
@@ -0,0 +1,33 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+//@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet {
+	
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+        PrintWriter out=resp.getWriter();
+		
+		ServletConfig cng=getServletConfig();
+		
+		String info=cng.getInitParameter("demo");
+		
+		out.println("Your data is::"+info);
+		
+	}
+
+}

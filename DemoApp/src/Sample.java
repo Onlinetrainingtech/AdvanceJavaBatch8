d504538512690a8d995dc71f9efe931b287a906a
OnlineTrainer <mohamedtechdata@gmail.com>
2022-03-20 19:06:26 +0530

    20-03-2022

---
+++ DemoApp/src/Sample.java
@@ -0,0 +1,21 @@
+class Demo
+{
+	void get()
+	{
+		System.out.println("get method...");
+	}
+}
+
+
+public class Sample {
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Welcome to Demo...");
+		Demo f1=new Demo();
+		f1.get();
+
+	}
+
+}
